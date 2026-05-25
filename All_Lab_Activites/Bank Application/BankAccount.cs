d3b8ae80055c201d437d76e58d05cea7353889f4
mhdmusaddiq <mhd.musaddiq2007@gmail.com>
2026-05-25 14:49:01 +0530

    Activites

---
+++ All_Lab_Activites/Bank Application/BankAccount.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+using static BankApplication.BankAccount;
+
+namespace BankApplication
+{
+    public class BankAccount
+    {
+
+        public string bankName;
+        public string accountHolderName;
+        public int accountNumber;
+        private decimal accountBalance;
+
+        public decimal AccountBalance
+        {
+            get { return this.accountBalance; }
+            set
+            {
+                if (value >= 0)
+                {
+                    accountBalance = value;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid balance");
+                }
+            }
+        }
+
+        public List<Transaction> transactions = new List<Transaction>();
+
+        public bool Deposite(decimal amount)
+        {
+            bool istrue = true;
+            while (istrue)
+            {
+
+                if (amount > 0)
+                {
+                    accountBalance += amount;
+                    istrue = false;
+
+                    Transaction t = new Transaction();
+
+                    t.type = "Deposit";
+                    t.amount = amount;
+                    t.date = DateTime.Now;
+
+                    transactions.Add(t);
+
+                    Console.WriteLine($"Your Account balance: {accountBalance}");
+                    return true;
+
+                   
+                }
+                else
+                {
+                    Console.WriteLine("Invalid ... TRY AGAIN...");
+                    return false;
+
+                }
+            }
+            return false;
+
+        }
+
+
+        public bool Withdraw(decimal wamount)
+        {
+            bool wtrue = true;
+            while (wtrue)
+            {
+
+                if (wamount >= 0)
+                {
+                    if (accountBalance >= wamount)
+                    {
+                        accountBalance -= wamount;
+
+                        Transaction t = new Transaction();
+
+                        t.type = "Withdraw";
+                        t.amount = wamount;
+                        t.date = DateTime.Now;
+
+                        transactions.Add(t);
+
+                        Console.WriteLine($"Your Account balance: {accountBalance}");
+                        return true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Balance is not enough");
+                        return false;
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("Invalid ... TRY AGAIN...");
+                    return false;
+                }
+            }
+            return false;
+        }
+
+
+
+        public void ShowTransaction()
+        {
+            
+            if(transactions.Count == 0)
+            {
+                Console.WriteLine("No transactions available");
+                return;
+            }
+             
+            Console.WriteLine("Transaction History");
+
+            foreach (Transaction t in transactions)
+            {
+                Console.WriteLine($"{t.date} | {t.type} | {t.amount}");
+            }
+            
+        }
+
+        
+
+        
+
+    }
+    public class Transaction
+    {
+        public string type;
+        public decimal amount;
+        public DateTime date;
+
+        //public Transaction(string type,decimal amount,DateTime date) {
+
+        //    this.type = type;
+        //    this.amount = amount;
+        //    this.date = date;
+        //}
+
+    }
+    
+
+}
+
+
+
---
+++ All_Lab_Activites/Bank Application/Program.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Security.Principal;
+using System.Xml.Linq;
+using System.Collections.Generic;
+using System.Transactions;
+namespace BankApplication
+{
+
+
+
+
+
+    internal class programs
+    {
+
+        static void WelcomeMsg()
+        {
+            Console.WriteLine("TIC BANK");
+            Console.WriteLine("HELLO WELCOME OUR BANK......");
+            Console.WriteLine("-----------------------------");
+        }
+
+        public static int Menu()
+        {
+            int menu;
+            Console.WriteLine(" 1)View Account \n 2)Check Balance \n 3)Deposit \n 4)Withdraw \n 5)Transaction History \n 6)Exit");
+            Console.Write("Select your option ? ");
+            menu = int.Parse(Console.ReadLine());
+            return menu;
+        }
+
+
+        public static void AccountDetails(BankAccount acc)
+        {
+            Console.Write("Enter Your BankName? ");
+            acc.bankName = Console.ReadLine();
+
+            Console.Write("Enter Your Accountholdername? ");
+            acc.accountHolderName = Console.ReadLine();
+
+            Console.Write("Enter Your Accountnumber? ");
+            acc.accountNumber = int.Parse(Console.ReadLine());
+
+            bool validBalance = false;
+
+            while (!validBalance)
+            {
+                Console.Write("Enter Your Accountbalnce: ");
+                decimal balance;
+
+                if (decimal.TryParse(Console.ReadLine(), out balance))
+                {
+                    acc.AccountBalance = balance;
+                    validBalance = true;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid balance... TRY AGAIN");
+                }
+
+            }
+        }
+
+
+            static void Main(string[] args)
+            {
+
+
+                WelcomeMsg();
+
+                BankAccount acc = new BankAccount();
+                AccountDetails(acc);
+
+
+
+                bool running = true;
+                while (running)
+                {
+                    int choice = Menu();
+
+                    Console.Clear();
+
+                    switch (choice)
+                    {
+                        case 1:
+                            Console.WriteLine($"Account Balance: {acc.bankName}");
+                            Console.WriteLine($"Account Holder Name: {acc.accountHolderName}");
+                            Console.WriteLine($"Account Number: {acc.accountNumber}");
+                            Console.WriteLine($"Account Balance: {acc.AccountBalance:F2}");
+                            break;
+                        case 2:
+                            Console.WriteLine($"Current Balance: {acc.AccountBalance:F2}");
+                            break;
+                        case 3:
+                            Console.WriteLine("Enter a deposit Amount: ");
+                            decimal amount;
+
+                            if (decimal.TryParse(Console.ReadLine(), out amount))
+                            {
+                                bool result = acc.Deposite(amount);
+                                if (result)
+                                {
+                                    Console.WriteLine("Deposite Successful");
+                                }
+                                else { Console.WriteLine("Deposite failier"); }
+                            }
+                            else
+                            {
+                                Console.WriteLine("Invalid amount");
+                            }
+
+                            break;
+
+                        case 4:
+
+                            Console.WriteLine("Enter a withdrawal Amount: ");
+                            decimal wamount;
+
+                            if (decimal.TryParse(Console.ReadLine(), out wamount))
+                            {
+                                bool withdrawResult = acc.Withdraw(wamount);
+
+                                if (withdrawResult)
+                                {
+                                    Console.WriteLine("Withdraw Successful");
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Withdraw Failed");
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("Invalid amount");
+                            }
+
+
+                            break;
+
+
+
+                        case 5:
+                            acc.ShowTransaction();
+                            break;
+
+
+                        case 6:
+                            Console.WriteLine("exit");
+                            running = false;
+                            break;
+
+
+
+                        default:
+                            Console.WriteLine("Invalid option");
+                            break;
+
+                    }
+
+
+
+                    //if (menu == 1)
+                    //{
+                    //    //viwe accunt
+                    //}
+                    //else if (menu == 2)
+                    //{
+                    //    //check balance
+                    //}
+                    //else if (menu == 3)
+                    //{
+                    //    //deposit
+                    //}
+                    //else if (menu == 4)
+                    //{
+                    //    //withdraw
+                    //}
+                    //else if (menu == 5) { 
+                    ////exit
+                    //running = false;
+                    //    Console.WriteLine("Exit");
+                    //}else
+                    //{
+                    //    Console.WriteLine("Invalid option...");
+                    //}
+
+
+
+
+
+                }
+            }
+        }
+    }
